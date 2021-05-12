6ca305f8188419645ae15dc4db9710101eefb321
peterMcP <36275626+peterMcP@users.noreply.github.com>
2021-05-13 00:01:14 +0200

    added base overview content
    starting content to vbo, ebo, vao, ,textures and print to screen quad

--- Engine/Code/engine.h
+++ Engine/Code/engine.h
@@ -14,6 +14,24 @@ typedef glm::ivec2 ivec2;
 typedef glm::ivec3 ivec3;
 typedef glm::ivec4 ivec4;
 
+struct VertexV3V2
+{
+    glm::vec3 pos;
+    glm::vec2 uv;
+};
+
+const VertexV3V2 vertices[] = {
+    { glm::vec3(-0.5, -0.5, 0.0), glm::vec2(0.0, 0.0)}, // bottom left vertex
+    { glm::vec3(0.5, -0.5, 0.0), glm::vec2(1.0, 0.0) }, // bottom right vertex
+    { glm::vec3(0.5, 0.5, 0.0), glm::vec2(1.0, 1.0)} , // top right vertex
+    { glm::vec3(-0.5, 0.5, 0.0), glm::vec2(0.0, 1.0) } // top left vertex
+};
+
+const u16 indices[] = {
+    0, 1, 2,
+    0, 2, 3
+};
+
 struct Image
 {
     void* pixels;
