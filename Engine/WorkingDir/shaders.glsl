e009d362ace63e1108310339b8a8292245f8f986
peterMcP <36275626+peterMcP@users.noreply.github.com>
2021-05-17 22:36:16 +0200

    fix specular

--- Engine/WorkingDir/shaders.glsl
+++ Engine/WorkingDir/shaders.glsl
@@ -121,7 +121,7 @@ void main()
 			ambient += lightContribution * ambientFactor * lightColor;
 
 			// specular
-			vec3 r = reflect(-lightDir, uNormal);
+			vec3 r = reflect(lightDir, uNormal);
 			float specComponent = pow(max(dot(normalize(vViewDir), r), 0.0), shininess);
 			specular += specComponent * specFactor * lightColor; 
 		}
