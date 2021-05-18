e98058f4e0b1e2c3dcc16cb3b60a33e54df8ff7a
Aurelio Gamarra <aurelio.gamarra@innovamat.com>
2021-05-18 20:23:10 +0200

    Fixed inverted parameters and added pbr shader

--- Engine/Code/engine.cpp
+++ Engine/Code/engine.cpp
@@ -264,29 +264,34 @@ void Init(App* app)
     // create some lights -----------------------------------------
     // directional front from up
     Light light = {};
-    light.color = { 1.0, 1.0, 1.0 };
-    light.direction = { 0, -1, -1 };
+    light.color = vec3(0.6);
+    light.direction = { 0, -1, -2 };
     light.type = LightType::LightType_Directional;
     app->lights.push_back(light);
     
-    // directional back from up
-    light.color = { 1.0, 1.0, 1.0 };
-    light.direction = { 0, -1, 1 };
-    app->lights.push_back(light);
+    //// directional back from up
+    //light.color = { 1.0, 1.0, 1.0 };
+    //light.direction = { 0, -1, 1 };
+    //app->lights.push_back(light);
 
     // point lights
-    light.color = { 0.0, 0.0, 1.0 };
-    light.position = { 0.0, 0.0, 6.0 };
+    light.color = vec3(23.47, 21.31, 20.79);
+    light.position = { 0.0, 1., -5.0 };
     light.type = LightType::LightType_Point;
     app->lights.push_back(light);
 
-    light.color = { 1.0, 0.0, 0.0 };
-    light.position = { -6.0, 0.0, 3.0 };
+    light.color = vec3(0.0, 0.0, 20.79);
+    light.position = { -6.0, 1.0, -5.0 };
     app->lights.push_back(light);
 
-    light.color = { 0.0, 1.0, 0.0 };
-    light.position = { 6.0, 0.0, 3.0 };
+    light.color = vec3(23.47, 0.0, 0.0);
+    light.position = { 6.0, 1.0, -5.0 };
     app->lights.push_back(light);
+
+
+    //light.color = { 0.0, 1.0, 0.0 };
+    //light.position = { 6.0, 0.0, 3.0 };
+    //app->lights.push_back(light);
 
     // ------------------------------------------------------------
 
@@ -442,7 +447,7 @@ void Init(App* app)
 
    Entity defaultElement = {};
    defaultElement.modelIndex = app->defaultModelsId[(int)DefaultModelType::Sphere];
-   defaultElement.worldMatrix = TransformWorldMatrix(vec3(-1.f, 4.f , 2.0f), vec3(90.f ,0.f ,0.f), vec3(2.f));
+   defaultElement.worldMatrix = TransformWorldMatrix(vec3(-1.f, 4.f , 2.0f), vec3(0.f ,0.f ,0.f), vec3(2.f));
    app->entities.push_back(defaultElement);
 
    Entity plane = {};
@@ -511,7 +516,7 @@ void Update(App* app)
         {
             app->globalParamsOffset = app->cbuffer.head;
 
-            PushVec3(app->cbuffer, app->camera.position);
+            PushVec3(app->cbuffer, -app->camera.position);
             PushUInt(app->cbuffer, app->lights.size());
 
             for (u32 i = 0; i < app->lights.size(); ++i)
@@ -522,7 +527,7 @@ void Update(App* app)
                 PushUInt(app->cbuffer, l.type);
                 PushVec3(app->cbuffer, l.color);
                 PushVec3(app->cbuffer, l.direction);
-                PushVec3(app->cbuffer, l.position);
+                PushVec3(app->cbuffer, -l.position);
             }
 
             app->globalParamsSize = app->cbuffer.head - app->globalParamsOffset;
--- Engine/WorkingDir/shaders.glsl
+++ Engine/WorkingDir/shaders.glsl
@@ -69,23 +69,23 @@ layout(binding = 1, std140) uniform LocalParams
 out vec2 vTexCoord;
 out vec3 vPosition; // in worldspace
 out vec3 vNormal;   // in worldspace
-out vec3 vViewDir;  // in worldspace
 
 void main()
 {
 	vTexCoord = aTexCoord;
 	vPosition = vec3(uWorldMatrix * vec4(aPosition, 1.0));
 	vNormal = vec3(uWorldMatrix * vec4(aNormal, 0.0));
-	vViewDir = uCameraPosition - vPosition;
 	gl_Position = uWorldViewProjectionMatrix * vec4(aPosition, 1.0);
 }
 
 #elif defined(FRAGMENT) ///////////////////////////////////////////////
 
+vec4 CalculateLightInternal(Light light, vec3 lightDirection, vec3 normal);
+vec4 CalculateDirectionalLight(Light light, vec3 normal);
+
 in vec2 vTexCoord;
 in vec3 vPosition;
 in vec3 vNormal;
-in vec3 vViewDir;
 
 uniform sampler2D uTexture;
 
@@ -98,71 +98,134 @@ layout(binding = 0, std140) uniform GlobalParams
 	Light uLight[16];
 };
 
+const float PI = 3.14159265359;
+  
+float DistributionGGX(vec3 N, vec3 H, float roughness);
+float GeometrySchlickGGX(float NdotV, float roughness);
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness);
+vec3 FresnelSchlick(float cosTheta, vec3 baseReflection);
+
+
 void main()
 {
-	vec3 uNormal = normalize(vNormal);
-	vec3 diffuse, ambient, specular;
+	vec3 N = normalize(vNormal); 
+    vec3 V = normalize(uCameraPosition - vPosition);
 
-	float ambientFactor = 0.2;
-	float diffuseFactor = 0.8;
-	float shininess = 20.0;
-	float specFactor = 0.7;
+    vec3  albedo = pow(texture(uTexture, vTexCoord).rgb , vec3(2.2));
+	float metallic = 0.1;
+	float roughness = 0.5;
+	float ao = 1.0;
 	
+	vec3 baseReflection = mix( vec3(0.04), albedo, metallic);
+
+	vec3 Lo = vec3(0.0);
+
 	for(int i = 0; i < uLightCount; ++i)
 	{
-		vec3 lightColor = uLight[i].color;
+		Light light = uLight[i];
 		
-		if(uLight[i].type == 0u) // directional
+		// Directional Light
+		if(light.type == 0u) 
 		{
-			vec3 lightDir = normalize(-uLight[i].direction);
-			float lightContribution = max(dot(uNormal, lightDir), 0.0);
+			vec3 L = normalize(-light.direction);
+			vec3 H = normalize(V + L);
+			vec3 radiance = light.color ;        
+			
+			// cook-torrance brdf
 
-			diffuse += lightContribution * diffuseFactor * lightColor;
-			ambient += lightContribution * ambientFactor * lightColor;
-
-			// specular
-			vec3 r = reflect(lightDir, uNormal);
-			float specComponent = pow(max(dot(normalize(vViewDir), r), 0.0), shininess);
-			specular += specComponent * specFactor * lightColor; 
+			float NDF = DistributionGGX(N, H, roughness);        
+			float G   = GeometrySmith(N, V, L, roughness);      
+			vec3 F    = FresnelSchlick(max(dot(H, V), 0.0), baseReflection);       
+			
+			vec3 kS = F;
+			vec3 kD = vec3(1.0) - kS;
+			kD *= 1.0 - metallic;	  
+			
+			vec3 numerator    = NDF * G * F;
+			float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+			vec3 specular     = numerator / max(denominator, 0.001);  
+			    
+			// add to outgoing radiance Lo
+			float NdotL = max(dot(N, L), 0.0);                
+			Lo += (kD * albedo / PI + specular) * radiance * NdotL; 
 		}
-		else // point light
+		// Point Light
+		else 
 		{
-			vec3 lightDir = normalize(uLight[i].position - vPosition);
-			float lightContribution = max(dot(uNormal, lightDir), 0.0);
+			// calculate per-light radiance
+			vec3 L = normalize(light.position - vPosition);
+			vec3 H = normalize(V + L);
+			float distance    = length(light.position - vPosition);
+			float attenuation = 1.0 / (distance * distance);
+			vec3 radiance     = light.color * attenuation;        
+			
+			// cook-torrance brdf
 
-			vec3 d = lightContribution * diffuseFactor * lightColor;
-			vec3 a = lightContribution * ambientFactor * lightColor;
-
-			// specular
-			vec3 r = reflect(-lightDir, uNormal);
-			float specComponent = pow(max(dot(normalize(vViewDir), r), 0.0), shininess);
-
-			// attenuation
-			float constant = 1.0;
-			float linear = 0.09;
-			float quadratic = 0.032;
-			float dist = length(uLight[i].position - vPosition);
-			float attenuation = 1.0 / (constant + linear * dist +
-								       quadratic * (dist * dist));
-			vec3 s = specComponent * specFactor * lightColor;
-
-			a *= attenuation;
-			d *= attenuation;
-			s *= attenuation;
-
-			diffuse += d;
-			ambient += a;
-			specular += s;
-
+			float NDF = DistributionGGX(N, H, roughness);        
+			float G   = GeometrySmith(N, V, L, roughness);      
+			vec3 F    = FresnelSchlick(max(dot(H, V), 0.0), baseReflection);       
+			
+			vec3 kS = F;
+			vec3 kD = vec3(1.0) - kS;
+			kD *= 1.0 - metallic;	  
+			
+			vec3 numerator    = NDF * G * F;
+			float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+			vec3 specular     = numerator / max(denominator, 0.001);  
+			    
+			// add to outgoing radiance Lo
+			float NdotL = max(dot(N, L), 0.0);                
+			Lo += (kD * albedo / PI + specular) * radiance * NdotL; 
 		}
 	}
-	
-	vec4 baseColor = texture(uTexture, vTexCoord);
-	vec4 objColor = baseColor * vec4(ambient, 1.0) + // ambient
-					baseColor * vec4(diffuse, 1.0) + // diffuse
-					baseColor * vec4(specular, 1.0); // specular
 
-	oColor = objColor;//vec4(diffuse, 1.0);
+	vec3 ambient = vec3(0.03) * albedo * ao;
+    vec3 color = ambient + Lo;
+	
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0/2.2));  
+
+	oColor =  vec4(color, 1.0);
+}
+
+vec3 FresnelSchlick(float cosTheta, vec3 baseReflection)
+{
+    return baseReflection + (1.0 - baseReflection) * pow(max(1.0 - cosTheta, 0.0), 5.0);
+}  
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a      = roughness*roughness;
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0000001);
+    float NdotH2 = NdotH*NdotH;
+	
+    float num   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+	
+    return num / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float num   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+	
+    return num / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V),  0.0000001);
+    float NdotL = max(dot(N, L), 0.0000001);
+    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+	
+    return ggx1 * ggx2;
 }
 
 #endif
@@ -173,3 +236,99 @@ void main()
 // long as you embrace them within an #ifdef block (as you can see above).
 // The third parameter of the LoadProgram function in engine.cpp allows
 // chosing the shader you want to load by name.
+
+//
+//vec4 CalculateLightInternal(Light light, vec3 lightDirection, vec3 normal)
+//{
+//	float ambientIntensity = 0.1;
+//	float diffuseIntensity = 0.4;
+//	float specularIntensity = 1.0;
+//	float specularPower = 32.0; // 1 to 32
+//
+//	vec4 ambientColor = vec4(light.color * ambientIntensity, 1.0); //
+//	float diffuseFactor = dot( normal, -lightDirection);
+//	vec4 diffuseColor = vec4(0.0);
+//	vec4 specularColor = vec4(0.0);
+//	
+//	if (diffuseFactor > 0.0) 
+//	{
+//		diffuseColor = vec4(light.color*  diffuseIntensity * diffuseFactor, 1.0) ; //
+//
+//		vec3 vertexToEye = normalize(uCameraPosition - vPosition);
+//		vec3 lightReflect = normalize(reflect(lightDirection, normal));
+//		float specularFactor = dot(vertexToEye, lightReflect);
+//
+//		if (specularFactor > 0.0) 
+//		{
+//			specularFactor = pow(specularFactor, specularPower);
+//			specularColor = vec4(light.color * specularIntensity * specularFactor, 1.0);
+//		}
+//	}
+//
+//	return (ambientColor + diffuseColor + specularColor) ;
+//}
+//
+//
+//vec4 CalculateDirectionalLight(Light light, vec3 normal)
+//{
+//    return CalculateLightInternal(light,  normalize(light.direction) , normal);
+//}
+//
+//void main()
+//{
+//	vec3 normal = normalize(vNormal);
+//
+//	
+//	vec4 totalLight = vec4(0.0);
+//
+//	for(int i = 0; i < uLightCount; ++i)
+//	{
+//		Light light = uLight[i];
+//		
+//		// Directional Light
+//		if(light.type == 0u) 
+//		{
+//			totalLight += CalculateDirectionalLight(light, normal);
+//		
+//		}
+//		// Point Light
+//		else 
+//		{
+////		    float constant = 1.0;
+////			float linear = 0.09;
+////			float quadratic = 0.032;
+////			float exponential = 0.032;
+////
+////			vec3 lightDirection = vPosition - light.position;
+////			float dist = length(lightDirection);
+////			lightDirection = normalize(lightDirection);
+////
+////			vec4 ambientColor = vec4(light.color * ambientIntensity, 1.0);
+////			float diffuseFactor = dot( normal, -lightDirection);
+////			vec4 diffuseColor = vec4(0.0);
+////			vec4 specularColor = vec4(0.0);
+////
+////			if (diffuseFactor > 0.0) 
+////			{
+////				diffuseColor = vec4(light.color*  diffuseIntensity * diffuseFactor, 1.0) ;
+////				vec3 lightReflect = normalize(reflect(lightDirection, normal));
+////				float specularFactor = dot(vViewDir, lightReflect);
+////
+////				if (specularFactor > 0.0) 
+////				{
+////					specularFactor = pow(specularFactor, specularPower);
+////					specularColor = vec4(light.color * specularIntensity * specularFactor, 1.0);
+////				}
+////			}
+////
+////			vec4 color = ambientColor + diffuseColor + specularColor ;
+////			float attenuation = constant + linear * dist + exponential * dist * dist;
+////			
+////			totalLight += color / attenuation;
+//		}
+//
+//
+//		oColor =  texture(uTexture, vTexCoord) * totalLight;
+//	}
+//}
+//
